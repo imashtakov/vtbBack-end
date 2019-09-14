316406fb02a62a6c39777784eb100ffc5e79e3c7
Pom4H <1@rom4.ru>
2019-09-14 23:16:54 +0300

    fix: getUserAddress handle

--- functions/src/endpoints.ts
+++ functions/src/endpoints.ts
@@ -2,6 +2,6 @@ const BASE_URL = 'http://89.208.84.235:31080/api/v1';
 
 const session = `${BASE_URL}/session`;
 
-const identifier = (identifier: string) => `${BASE_URL}/identifier/${identifier}`;
+const identifier = (identifier: string) => `${BASE_URL}/account/identifier/${identifier}`;
 
 export default { session, identifier };
--- functions/src/handlers.ts
+++ functions/src/handlers.ts
@@ -2,17 +2,18 @@ import axios from 'axios';
 import endpoint from './endpoints';
 import { createHash } from 'crypto';
 
-const axiosConfig = { headers: { deviceId: 'metadevs', FPSID: null } };
+const deviceId = 'metadevs';
 
 type User = {
     address: string;
 }
 
 const getUserAddress = async (username: string): Promise<User> => {
-    const { data: sessionData } = await axios.post(endpoint.session, {}, axiosConfig);
+    const { data: sessionData } = await axios.post(endpoint.session, { deviceId });
+    const axiosConfig = { headers: { FPSID: null } };
     axiosConfig.headers.FPSID = sessionData.data;
     const identifier = createHash('sha256').update(Buffer.from(username)).digest('hex');
-    const { data: userData } = await axios.post(endpoint.identifier(identifier), {}, axiosConfig);
+    const { data: userData } = await axios.get(endpoint.identifier(identifier), axiosConfig);
     return userData.data.address;
 }
 
