5b530a4e67f34d7617c9ff6a2e6d023dea8417ac
Pom4H <1@rom4.ru>
2019-09-15 10:13:58 +0300

    feat: add createPayment types

--- functions/src/types.d.ts
+++ functions/src/types.d.ts
@@ -9,15 +9,15 @@ export type User = {
 export type Payment = {
   id: string;
   participants: {
-      address: string;
-      amount: number;
-      /**
-       * 0 - Отменен
-       * 1 - Успешно закрыт
-       * 2 - В процессе
-       */
-      status: '0' | '1' | '2';
-      invoiceNumber: string;
+    address: string;
+    amount: number;
+    /**
+     * 0 - Отменен
+     * 1 - Успешно закрыт
+     * 2 - В процессе
+     */
+    status: '0' | '1' | '2';
+    invoiceNumber: string;
   }[];
   description: string;
   ownerAmount: number;
@@ -34,7 +34,20 @@ type PaymentList = {
   list: Payment[];
 };
 
+type Payer = { address: string, amount: number };
+
+type PaymentPayload = {
+  username: string;
+  payment: {
+    participants: Payer[];
+    description: string;
+    ownerAmount: number;
+    overallCost: number;
+  };
+}
+
 export interface MetaDevsAPI {
   getUserAddress(username: string): Promise<ApiResponse<User>>;
-  getUserPayments(username: string): Promise<ApiResponse<PaymentList>>
+  getUserPayments(username: string): Promise<ApiResponse<PaymentList>>;
+  createPayment(paymentPayload: PaymentPayload): Promise<ApiResponse<null>>;
 }
